7b43e0d3a9de2a788699a53dc43485f419e45b66
Yehdua.Shapira <Yehuda.Shapira@4xdrm.io>
2022-08-14 10:18:38 +0300

    add filepath

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -48,6 +48,11 @@ local location = {
 	padding = 0,
 }
 
+local filename = {
+  "filename",
+  path = 1,
+}
+
 -- cool function for progress
 local progress = function()
 	local current_line = vim.fn.line(".")
@@ -74,7 +79,7 @@ lualine.setup({
 	sections = {
 		lualine_a = { branch, diagnostics },
 		lualine_b = { mode },
-		lualine_c = {},
+		lualine_c = { filename },
 		-- lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_x = { diff, spaces, "encoding", filetype },
 		lualine_y = { location },
