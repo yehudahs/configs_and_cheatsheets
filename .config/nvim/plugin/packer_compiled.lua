0525206158aada104dced67ae87b3f97d12b1049
Yehdua.Shapira <Yehuda.Shapira@4xdrm.io>
2022-08-14 08:55:53 +0300

    update grubox theme

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -139,6 +139,11 @@ _G.packer_plugins = {
     path = "/Users/yehuda/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
+  gruvbox = {
+    loaded = true,
+    path = "/Users/yehuda/.local/share/nvim/site/pack/packer/start/gruvbox",
+    url = "https://github.com/morhetz/gruvbox"
+  },
   ["impatient.nvim"] = {
     loaded = true,
     path = "/Users/yehuda/.local/share/nvim/site/pack/packer/start/impatient.nvim",
